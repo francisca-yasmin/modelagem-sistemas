42c0166bb5e84460651859ae0d4210a267685dbe
EA - Francisca Yasmin Freitas De Almeida <francisca_yasmin@acad.unasp.edu.br>
2026-03-12 22:24:32 -0300

    updates login.js

--- login/login.js
+++ login/login.js
@@ -14,44 +14,49 @@ const app = express();
 
 app.use(session({
     secret: 'secret',
-    resave: true, //salva informacoes no hook
-    saveUninitialized: true //cria a sessao mesmo sem informacao 
+    resave: true, // salva informacoes no hook
+    saveUninitialized: true // cria a sessao mesmo sem informacao
 }));
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.json(__dirname, 'static')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
 
-app.get('/', function(request, response){
-    response.sendFile(path.join(__dirname+ '/login.html'))
+app.get('/', function (request, response) {
+    response.sendFile(path.join(__dirname + '/login.html'));
 });
 
-app.post('/auth', function(request, response){
+app.post('/auth', function (request, response) {
     let username = request.body.username;
     let password = request.body.password;
 
-    if(username && password){
-        connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields){
-            if (error) throw error;
-            if(results.length > 0){
-                request.session.loggedin = true;
-                request.session.username = username; //deixar mais dinamico
-                response.redirect('/home');
-            }else {
-                response.send('usuário ou senha incoretos')
+    if (username && password) {
+        connection.query(
+            'SELECT * FROM accounts WHERE username = ? AND password = ?',
+            [username, password],
+            function (error, results, fields) {
+                if (error) throw error;
+
+                if (results.length > 0) {
+                    request.session.loggedin = true;
+                    request.session.username = username;
+                    response.redirect('/home');
+                } else {
+                    response.send('usuário ou senha incorretos');
+                }
+                response.end();
             }
-            response.end();
-        });   
-    }else{
+        );
+    } else {
         response.send('preencha o usuario e a senha');
         response.end();
     }
-)};
+}); 
 
-app.get('/home', function(request, response){
-    if(request.session.loggedin){
+app.get('/home', function (request, response) {
+    if (request.session.loggedin) {
         response.send('bem vindo, ' + request.session.username + '!');
-    }else{
+    } else {
         response.send('faça o login para acessar essa pagina!!');
     }
     response.end();
