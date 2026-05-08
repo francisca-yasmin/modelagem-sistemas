2b971773060f1afa107b67dd8b83920899d3b6e2
EA - Arthur Oliveira Maia <maia_arthur01@acad.unasp.edu.br>
2026-05-07 22:35:38 -0300

    aula do dia 07 - conectando o banco por linha de codigo

---
+++ atividade 07/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser")
+const connection = require("./database/database")
+
+connection
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get("/",(req, res) => {
+    res.render("index");
+});
+
+app.get("/perguntar",(req, res) => {
+    res.render("perguntar");
+});
+
+app.post("/salvarpergunta", (req,res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulário Recebido titulo=" +titulo+" "+ "desc="+descricao);
+})
+
+app.listen(8080, ()=>{
+    console.log("App rodando");
+});
